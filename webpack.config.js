2f5062a048e233c58dcbf54497efb4e03f8c3c4d
n.ageeva <n.ageeva@skbkontur.ru>
2019-12-04 00:16:10 +0500

    production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,59 @@
 const path = require('path');
 const bundlePath = path.resolve(__dirname, 'build');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: "development",
-    entry: './src/index.tsx',
-    output: {
-        filename: 'bundle.js',
-        path: bundlePath,
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"]
-    },
+module.exports = (env, argv) => {
+    const IS_DEV = argv.mode !== 'production';
+    
+    return {
 
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                loader: "ts-loader"
-            },
-            {
-                test: /\.(css|less)$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                localIdentName: '[local]___[hash:base64:4]',
-                            },
-                            sourceMap: true,
-                        }
-                    },
-                    {
-                        loader: 'less-loader',
-                    },
-                ],
-            },
+        entry: './src/index.tsx',
+        output: {
+            filename: IS_DEV ? 'bundle.js' : 'bundle___[hash].js',
+            path: bundlePath,
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"]
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    include: /src/,
+                    loader: "ts-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"]
+                },
+                {
+                    test: /\.(less)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                                modules: {
+                                    localIdentName: IS_DEV ? '[local]___[hash:base64:4]' : '[hash:base64:4]'
+                                }
+                            }
+                        },
+                        'less-loader',
+                    ],
+                },
+            ]
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: IS_DEV ? 'main.css' : 'main___[hash].css',
+            }),
+            new HtmlWebpackPlugin({
+                template: './resources/index.html',
+            })
         ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './resources/index.html',
-        })
-    ]
+    }
 };
