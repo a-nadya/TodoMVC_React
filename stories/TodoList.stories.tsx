aeff3f6b17aaf0882322a1833561d60edd7f3afe
n.ageeva <n.ageeva@skbkontur.ru>
2019-12-11 14:56:20 +0500

    add story

---
+++ stories/TodoList.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import { TodoList } from "../src/components/TodoList/TodoList";
+
+export default { title: "TodoList" };
+
+export const list = () => (
+    <TodoList
+        todos={{
+            "324243": { active: true, text: "lalalalala" },
+            "444": { active: false, text: "qwer" },
+            "423223": { active: true, text: "DDFDFFDDFDFFD" },
+        }}
+        onCheck={() => {}}
+        onDelete={() => {}}
+    />
+);
