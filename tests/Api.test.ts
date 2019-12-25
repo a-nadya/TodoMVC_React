b4652731912c933121243b0bf033fe92bd4e8de7
n.ageeva <n.ageeva@skbkontur.ru>
2019-12-25 18:16:10 +0500

    add tests

--- tests/Api.test.ts
+++ tests/Api.test.ts
@@ -4,11 +4,78 @@ import "isomorphic-fetch";
 // tslint:disable-next-line:no-import-side-effect
 import "mocha";
 
-import { api } from "../src/api/Api";
+import { Api } from "../src/api/Api";
 
-describe("Get", () => {
+describe("select", () => {
+    it("should return error if binId is incorrect", async () => {
+        const api = new Api("757590475757");
+        await expect(api.select()).to.throw;
+    });
+
     it("should return correct data", async () => {
+        const api = new Api("5e008e18bda54254c5f17a4f");
         const response = await api.select();
-        expect(response).eql({});
+        expect(response).eql({
+            "12345": {
+                text: "text1",
+                active: true,
+            },
+            "2138777657": {
+                text: "text2",
+                active: true,
+            },
+        });
+    });
+
+    it("should return entity Empty if list of todo is empty", async () => {
+        const api = new Api("5e00b1f7ec09451045d7bec2");
+        const response = await api.select();
+        expect(response).eql({
+            empty: true,
+        });
+    });
+});
+
+describe("update", () => {
+    it("should return error if binId is incorrect", async () => {
+        const api = new Api("1234543456");
+        await expect(
+            api.update({
+                "12345": {
+                    text: "text1",
+                    active: true,
+                },
+                "2138777657": {
+                    text: "text2",
+                    active: true,
+                },
+            })
+        ).to.throw;
+    });
+
+    it("should return nothing if update was success", async () => {
+        const api = new Api("5e035fa332ac62710773869e");
+        const response = await api.update({
+            "11111": {
+                text: "new",
+                active: false,
+            },
+        });
+        expect(response).eql(undefined);
+        expect(await api.select()).eql({
+            "11111": {
+                text: "new",
+                active: false,
+            },
+        });
+    });
+
+    it("should set entity Empty if list of todo is empty", async () => {
+        const api = new Api("5e0360bde3eeeb70eb967cf8");
+        const response = await api.update({});
+        expect(response).eql(undefined);
+        expect(await api.select()).eql({
+            empty: true,
+        });
     });
 });
